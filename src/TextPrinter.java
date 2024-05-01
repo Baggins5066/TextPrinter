9057f53e5a10a9d317f37ae755e9cc025bb9f5f2
Baggins5066 <little0jasper@gmail.com>
2024-05-01 11:14:03 -0400

    Text printing speed is now randomized between characters.

--- src/TextPrinter.java
+++ src/TextPrinter.java
@@ -1,13 +1,14 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class TextPrinter {
     public static void main(String[] args) throws Exception {
-
         String text = "The quick brown fox jumps over the lazy dog.";
         
         System.out.println();
         System.out.print("\033[32m");
         for (int i = 0; i < text.length(); i++) {
             System.out.print(text.substring(i,i+1));
-            Thread.sleep(25);
+            Thread.sleep(ThreadLocalRandom.current().nextInt(10,150));
         }
         System.out.print("\033[0m");
         System.out.println();
